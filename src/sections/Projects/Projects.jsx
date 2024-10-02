cae533717fa4dda0a80356e1b82908b6d6129125
egifthahir <egif_thahir@yahoo.com>
2024-10-02 12:21:14 +0700

    project revised

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -42,7 +42,7 @@ function Projects() {
                 <ProjectCard 
                     src={tlcIcon}
                     link="https://github.com/egifthahir/JCDS_CAPSTONE_2"
-                    h3="New York City - Taxi & Limousine Commission Trip Record Analysis"
+                    h3="New York City - TLC Trip Record Analysis"
                     p="Data Analysis and Statistical Evaluation"
                 />      
                 <ProjectCard 
